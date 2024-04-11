a00ef7bb9263912f9275efd738f30c934e6441a2
DuaneHuynh <dhuynh1@ufl.edu>
2024-04-11 12:53:23 -0400

    Encoder + Decoder
    Duane's encode and decode

--- DCH_lab9.py
+++ DCH_lab9.py
@@ -1,4 +1,11 @@
-def decoder:
+#this is Duane Huynh's code
+def encoder(password):
+    encoded_pass=''
+    for char in password:
+        encoded_pass = encoded_pass+ str(int(char)+3)%10
+    return encoded_pass
+
+def decoder(password):
     decoded_pass = ''
     for char in password:
         decoded_pass = decoded_pass + str((int(char)-3)%10)
