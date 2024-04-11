0e0de7816c803669a61dda8f919fec0c92465d37
DuaneHuynh <dhuynh1@ufl.edu>
2024-04-11 13:21:58 -0400

    Full code

--- DCH_lab9.py
+++ DCH_lab9.py
@@ -2,7 +2,7 @@
 def encoder(password):
     encoded_pass=''
     for char in password:
-        encoded_pass = encoded_pass+ str(int(char)+3)%10
+        encoded_pass = encoded_pass+ str((int(char)+3)%10)
     return encoded_pass
 
 
@@ -11,3 +11,18 @@ def decode(password):
     for char in password:
         decoded_password += str((int(char) + 7) % 10)
     return decoded_password
+
+if __name__ == '__main__':
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        choice = input("PLease enter an option: ")
+        if choice == '1':
+            password = input("Please enter your password to encode: ")
+            encoded_password = encoder(password)
+            print("Your password has been encoded and stored!")
+
+        if choice == '2':
+            print("The encoded passowrd is", encoded_password, "and the original password is", password )
+
+        if choice == '3':
+            break
